22cc01e26cc5e6c61f8d0c4d6c29c568c781b57a
David THIBAU <david.thibau@gmail.com>
2023-02-19 15:10:24 +0100

    gateway as resource server

--- gateway/src/main/java/org/formation/GatewayApplication.java
+++ gateway/src/main/java/org/formation/GatewayApplication.java
@@ -2,12 +2,21 @@ package org.formation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
+@EnableWebFluxSecurity
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public SecurityWebFilterChain securitygWebFilterChain(ServerHttpSecurity http) {
+		return http.authorizeExchange().anyExchange().authenticated().and().oauth2ResourceServer(v -> v.jwt()).csrf().disable().build();
+	}
 }
